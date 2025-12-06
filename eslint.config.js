5e71f8a814d4cf72c1e3d258fd6ed219c0c8d325
ptdtw <tangperry8787@gmail.com>
2025-12-06 23:32:31 +0800

    feat: update nextjs

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import nextConfig from "eslint-config-next";
 
-export default [
-  ...nextConfig,
-  {
-    rules: {
-      "@next/next/no-img-element": "error",
-    },
+export default [...nextCoreWebVitals, ...nextTypescript, ...nextConfig, {
+  rules: {
+    "@next/next/no-img-element": "error",
   },
-];
+}, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
